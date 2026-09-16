443bda22e7f73236e8c047beef35c9825400073d
Kira Gami <hishgee.kira@gmail.com>
2026-09-16 19:43:04 +0800

    feat: initialize Spring Boot project with PostgreSQL configuration (#1)

---
+++ src/main/java/mn/icode/SpringBootClinicCapstoneApplication.java
@@ -0,0 +1,13 @@
+package mn.icode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClinicCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClinicCapstoneApplication.class, args);
+	}
+
+}
