2e62e18bf994c5d64fa337133e15370164dbf7a8
Khishigbayar <hishgee.kira@gmail.com>
2026-09-21 16:48:23 +0800

    feat: implement pagination for doctors list and search results (#18)

--- src/main/java/mn/icode/repository/DoctorRepository.java
+++ src/main/java/mn/icode/repository/DoctorRepository.java
@@ -3,8 +3,6 @@ package mn.icode.repository;
 import mn.icode.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long>, JpaSpecificationExecutor<Doctor> {
 }
